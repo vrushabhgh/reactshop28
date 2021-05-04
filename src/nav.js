3a001ba56a1d86cd22bdc0f870b2214810b9fadf
FIRST_NAME LAST_NAME <vrushabhgawande63@gmail.com>
2021-05-04 22:26:35 +0530

    inital

--- src/nav.js
+++ src/nav.js
@@ -2,7 +2,7 @@ export default function Navbar(){
     return <>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand">Shope Homepage</a>
+            <h2 class="navbar-brand">Shope Homepage</h2>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07"
                 aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
